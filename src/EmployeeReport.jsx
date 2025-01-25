32696dff44d492c5ccc5a905b2aa2975d1f496ad
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2025-01-24 22:53:38 -0800

    deleted incorrect files and added updated module 7 files

--- src/EmployeeReport.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function EmployeeReport() {
-    return (
-        <div>
-            <h1>This is a placeholder for the employee report.</h1>
-        </div>
-    )
-}
